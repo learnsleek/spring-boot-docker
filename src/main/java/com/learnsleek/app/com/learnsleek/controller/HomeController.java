5b8080db8e6fb8469a7574b40ba2b3902fce92f1
amit <mailamiton@gmail.com>
2019-08-02 09:21:36 +0530

    added chanegs for docker

--- src/main/java/com/learnsleek/app/com/learnsleek/controller/HomeController.java
+++ src/main/java/com/learnsleek/app/com/learnsleek/controller/HomeController.java
@@ -1,16 +1,21 @@
 package com.learnsleek.app.com.learnsleek.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping(path = "/")
 public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @GetMapping(path="/", produces = "application/json")
     public String getHome()
     {
-
+        logger.debug("Called Hello World !!");
         return "Hello World!";
     }
 }
